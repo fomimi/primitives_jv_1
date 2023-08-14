0c870e56a5f399494c6f146f9c6978ffd5aead66
Ekaterina Fomicheva <katerinafomi4eva@gmail.com>
2023-08-14 23:15:32 +0300

    расчёт начисленных бонусов

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 789;
+        int priceForMile = 20;
+        int miles = cost/priceForMile;
+        System.out.println("Количество начисленных миль: " + miles);
+    }
+}
